157b43b78eb7d86d20bb73cffd4eaa3b7869e3cb
willy23martin <william.chavez@correo.icesi.edu.co>
2021-01-26 16:20:56 -0500

    Java-Runnable

---
+++ spring-boot-mvc/src/main/java/openweb/wmc/java/eight/concurrency/RunnableTest.java
@@ -0,0 +1,24 @@
+package openweb.wmc.java.eight.concurrency;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RunnableTest {
+
+	public static void main(String[] args) {
+		List<String> contactPhoneNumbersSetA = Arrays.asList("0183333333", "0333636", "06894656", "06894657");
+		List<String> contactPhoneNumbersSetB = Arrays.asList("03535","03529", "0130085656", "0956564");
+		List<String> contactPhoneNumbersSetC = Arrays.asList("0125650408", "0857474","0857475",  "00000000");
+		
+		Thread phoneNumberClassificationSetA = new Thread(new CustomRunnable(1, contactPhoneNumbersSetA));
+		phoneNumberClassificationSetA.start();
+		
+	    Thread phoneNumberClassificationSetB = new Thread(new CustomRunnable(2, contactPhoneNumbersSetB));
+		phoneNumberClassificationSetB.start();
+		
+		Thread phoneNumberClassificationSetC = new Thread(new CustomRunnable(3, contactPhoneNumbersSetC));
+		phoneNumberClassificationSetC.start();
+		
+	}
+
+}
