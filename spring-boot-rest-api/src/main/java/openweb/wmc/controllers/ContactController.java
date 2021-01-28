e8ae25b890409ca558273fc582b73132533965d6
willy23martin <william.chavez@correo.icesi.edu.co>
2021-01-28 06:09:46 -0500

    SpringBoot-REST API-Contact API-GET/POST/PUT/DELETE

--- spring-boot-rest-api/src/main/java/openweb/wmc/controllers/ContactController.java
+++ spring-boot-rest-api/src/main/java/openweb/wmc/controllers/ContactController.java
@@ -1,9 +1,15 @@
 package openweb.wmc.controllers;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,25 +19,57 @@ import openweb.wmc.model.Contact;
 import openweb.wmc.model.ContactModel;
 
 /**
- * @RestController = @Controller + @ResponseBody: 
- * Returns the body in JSON format by using a HttpMessagesConverter
+ * @RestController = @Controller + @ResponseBody: Returns the body in JSON
+ *                 format by using a HttpMessagesConverter
  * @author ingmartic
  * @reference: https://openwebinars.net/academia/aprende/api-rest-spring-boot/7235/
  */
-@RestController 
+@RestController
 public class ContactController {
-	
+
 	@Autowired
 	private ContactModel model;
-	
-	@RequestMapping(value="/contacts", method=RequestMethod.GET)
-	public List<Contact> getContacts(){
+
+	@RequestMapping(value = "/contacts", method = RequestMethod.GET)
+	public List<Contact> getContacts() {
 		return this.model.loadContacts();
 	}
-	
-	@GetMapping(value="/contact")
-	public Contact getContact(@RequestParam(value="id", defaultValue="1") String id) {
-		return this.model.getContacts().stream().filter(c->c.getId().equals(id)).findAny().get();
+
+	@GetMapping(value = "/contact")
+	public Contact getContactParam(@RequestParam(value = "id", defaultValue = "1") String id) {
+		return this.model.getContacts().stream().filter(c -> c.getId().equals(id)).findAny()
+				.orElseGet(new Supplier<Contact>() {
+					@Override
+					public Contact get() {
+						return new Contact(id, "no exist", "no exist", "does not exist");
+					}
+				});
+	}
+
+	@GetMapping(value = "/contacts/{id}")
+	public Contact getContact(@PathVariable(value = "id") String id) {
+		return this.model.getContacts().stream().filter(c -> c.getId().equals(id)).findAny()
+				.orElseGet(new Supplier<Contact>() {
+					@Override
+					public Contact get() {
+						return new Contact(id, "no exist", "no exist", "does not exist");
+					}
+				});
+	}
+
+	@PostMapping(value = "/contacts")
+	public Contact postContact(@RequestBody Contact contact) {
+		return this.model.createContact(contact);
+	}
+
+	@PutMapping(value = "/contacts/{id}")
+	public Contact updateContact(@RequestBody Contact contact, @PathVariable(value = "id") String id) {
+		return this.model.updateContact(contact, id);
 	}
 	
+	@DeleteMapping(value="/contacts/{id}")
+	public Contact deleteContact(@PathVariable String id) {
+		return this.model.deleteContact(id);
+	}
+
 }
--- spring-boot-rest-api/src/main/java/openweb/wmc/model/ContactModel.java
+++ spring-boot-rest-api/src/main/java/openweb/wmc/model/ContactModel.java
@@ -2,6 +2,7 @@ package openweb.wmc.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
@@ -11,18 +12,47 @@ import org.springframework.stereotype.Service;
 public class ContactModel {
 
 	private List<Contact> contacts;
-	
+
 	public ContactModel() {
-		this.contacts = new ArrayList<Contact>();	
+		this.contacts = new ArrayList<Contact>();
 		this.contacts.add(new Contact("1", "Name 1", "name1@subdomain.domain", "0398765656"));
 		this.contacts.add(new Contact("2", "Name 2", "name2@subdomain.domain", "0298765656"));
 		this.contacts.add(new Contact("3", "Name 3", "name3@subdomain.domain", "0198765656"));
 	}
 
-	public synchronized List<Contact> loadContacts(){
+	public List<Contact> loadContacts() {
 		return this.contacts;
 	}
-	
+
+	public synchronized Contact createContact(Contact newContact) {
+		if (!this.contacts.stream().filter(c -> c.getId().equals(newContact.getId())).findAny().isPresent()) {
+			this.contacts.add(newContact);
+		}
+		return this.contacts.stream().filter(c -> c.getId().equals(newContact.getId())).findAny().get();
+	}
+
+	public synchronized Contact updateContact(Contact contact, String id) {
+		if (this.contacts.stream().filter(c -> c.getId().equals(id)).findAny().isPresent()) {
+			this.contacts.removeIf(c -> c.getId().equals(id));
+			this.contacts.add(contact);
+		}
+		return this.contacts.stream().filter(c -> c.getId().equals(id)).findAny().orElseGet(new Supplier<Contact>() {
+			@Override
+			public Contact get() {
+				return new Contact(id, "no exist", "no exist", "does not exist");
+			}
+		});
+	}
+
+	public synchronized Contact deleteContact(String id) {
+		Contact deletedContact = null;
+		if (this.contacts.stream().filter(c -> c.getId().equals(id)).findAny().isPresent()) {
+			deletedContact = this.contacts.stream().filter(c -> c.getId().equals(id)).findAny().get();
+			this.contacts.removeIf(c -> c.getId().equals(id));
+		}
+		return deletedContact == null ? new Contact(id, "no exist", "no exist", "does not exist") : deletedContact;
+	}
+
 	public List<Contact> getContacts() {
 		return contacts;
 	}
@@ -30,5 +60,5 @@ public class ContactModel {
 	public void setContacts(List<Contact> contacts) {
 		this.contacts = contacts;
 	}
-	
+
 }
