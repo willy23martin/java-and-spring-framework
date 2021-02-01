1d5f876d8cddf0003974588dde7771e96a415771
willy23martin <william.chavez@correo.icesi.edu.co>
2021-02-01 17:15:06 -0500

    Java-SpringBoot-REST API with Error Handling and Global Config CORS as a Bean

--- spring-boot-rest-api/src/main/java/openweb/wmc/config/JavaConfig.java
+++ spring-boot-rest-api/src/main/java/openweb/wmc/config/JavaConfig.java
@@ -3,6 +3,8 @@ package openweb.wmc.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class JavaConfig {
@@ -10,6 +12,19 @@ public class JavaConfig {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	// Cross-Origin Resource Sharing in global configuration as a Bean.
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				// registry.addMapping("/**");
+				registry.addMapping("/contacts/**").allowedOrigins("http://localhost:4200")
+						.allowedMethods("GET", "POST").maxAge(3600);
+			}
+		};
 	}
 	
 }
